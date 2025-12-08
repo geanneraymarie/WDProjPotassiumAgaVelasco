b8d692e26e2cd178a6c8a00b8acf1d5bdb357ae5
maggie-velasco <b2029jmbvelasco@pshs.edu.ph>
2025-12-08 20:50:37 +0800

    Update platypusadventures_k01k28p3.js

--- platypusadventures_k01k28p3.js
+++ platypusadventures_k01k28p3.js
@@ -6,11 +6,11 @@ function problem3() {
           case 1:
                let problem31 = prompt("You have 3 red gloves, 6 white gloves, and 4 black gloves. Suddenly, the lights went out, and you couldn't see anything. How many gloves should you take to make sure you have at least one pair of matching gloves?", "Type your answer here");
                if (problem31 == null || problem31 == "") {
-                    lastOutput = "Enter the fricking password!";
+                    lastOutput = "You didn't enter anything :((";
                }
                else {
                     if(problem31 == "4") {
-                         lastOutput = "Correct! >>proceeds to program";
+                         lastOutput = "Correct!";
                     }
                     else {
                          lastOutput = "Incorrect.Try again";
@@ -20,11 +20,11 @@ function problem3() {
           case 2:
                let problem32 = prompt("A mother is 21 years older than her daughter now. Eight years ago, she was 4 times as old as her daughter. What is the sum of the ages of mother and daughter now?", "Type your answer here");
                if (problem32 == null || problem32 == "") {
-                    lastOutput = "Enter the fricking password!";
+                    lastOutput = "You didn't enter anything :((";
                }
                else {
                     if(problem32 == "51") {
-                         lastOutput = "Correct! >>proceeds to program";
+                         lastOutput = "Correct!";
                     }
                     else {
                          lastOutput = "Incorrect.Try again";
@@ -34,11 +34,11 @@ function problem3() {
           case 3:
                let problem33 = prompt("Combine (don't add) the numerical result of these 3 cases to get the answer:  CASE 1: The month of Valentines, or also known as the only prime even number     CASE 2: 4 years later, Kas is 10 times older than she was 14 years ago. How old is Kas now?     CASE 3: sqrt(4) - sqrt(100) + (4 x 5) - (3^2  - 3)", "00000");
                if (problem33 == null || problem33 == "") {
-                    lastOutput = "Enter the fricking password!";
+                    lastOutput = "You didn't enter anything :((";
                }
                else {
                     if(problem33 == "21610") {
-                         lastOutput = "Correct! >>proceeds to program";
+                         lastOutput = "Correct!";
                     }
                     else {
                          lastOutput = "Incorrect.Try again";
@@ -48,11 +48,11 @@ function problem3() {
           case 4:
                let problem34 = prompt("A pizza slice has an arc of 2/5 pi cm. It's central angle is 12 degrees. How long is the radius of the slice in cm? (Hint: Arc = (angle/360) x 2 pi r)", "Type your answer here");
                if (problem34 == null || problem34 == "") {
-                    lastOutput = "Enter the fricking password!";
+                    lastOutput = "You didn't enter anything :((";
                }
                else {
                     if(problem34 == "6") {
-                         lastOutput = "Correct! >>proceeds to program";
+                         lastOutput = "Correct!";
                     }
                     else {
                          lastOutput = "Incorrect.Try again";
