675f199022dde9fc1a4dc38cbcd8665e4910db18
maggie-velasco <b2029jmbvelasco@pshs.edu.ph>
2025-12-15 20:01:51 +0800

    Create casefiles.js

---
+++ casefiles.js
@@ -0,0 +1,55 @@
+function l1c1(){
+    document.getElementById("button1").onclick=function(){
+        document.getElementById("button2").style.display="block";
+    }
+    document.getElementById("button2").onclick=function(){
+        document.getElementById("button3").style.display="block";
+    }
+    document.getElementById("button3").onclick=function(){
+        document.getElementById("button4").style.display="block";
+    }
+    document.getElementById("button4").onclick=function(){
+        document.getElementById("button5").style.display="block";
+    }
+    document.getElementById("button5").onclick=function(){
+        document.getElementById("button6").style.display="block";
+    }
+    document.getElementById("button6").onclick=function(){
+        document.getElementById("button7").style.display="block";
+    }
+    document.getElementById("button7").onclick=function(){
+        document.getElementById("button8").style.display="block";
+    }
+    document.getElementById("button8").onclick=function(){
+        document.getElementById("button9").style.display="block";
+    }
+    document.getElementById("button9").onclick=function(){
+        document.getElementById("button10").style.display="block";
+    }
+    document.getElementById("button10").onclick=function(){
+        document.getElementById("button11").style.display="block";
+    }
+    document.getElementById("button11").onclick=function(){
+        document.getElementById("button12").style.display="block";
+    }
+    document.getElementById("button12").onclick=function(){
+        document.getElementById("lastOne").style.display="block";
+    }
+    
+
+}
+function l1c2(){
+
+}
+function l1c3(){
+
+}
+function l2c1(){
+
+}
+function l2c2(){
+
+}
+function l3(){
+
+}
