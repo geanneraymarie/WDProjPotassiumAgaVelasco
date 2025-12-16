6d9e24e95a3f18c2b03ec05d370ae4e39273434c
maggie-velasco <b2029jmbvelasco@pshs.edu.ph>
2025-12-16 18:05:42 +0800

    Update extras.js

--- extras.js
+++ extras.js
@@ -1,10 +1,2 @@
 //this is for the copyright part: it outputs the current year
 document.getElementById("year").innerHTML = new Date().getFullYear();
-
-/*function playMusic(){
-    const music = document.getElementById("play");
-    document.addEventListener("click", () => {
-        music.muted = false;
-        music.play();
-    }, { once: true });
-}*/
